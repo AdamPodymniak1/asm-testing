e4aa0b716b45f4cb50ce0c4bf4a6ade6884ff0f5
AdamPodymniak1 <kamilgrzecznarowski@gmail.com>
2026-03-05 23:35:36 +0100

    playing with hello world program

--- main.s
+++ main.s
@@ -3,8 +3,13 @@
 .data
 
 hellowrld: .ascii "HELLO, WORLD!" # CREATE DATA POINT
-newline: .byte 0x0A
-len = . - hellowrld
+newline: .byte 0x0A # ADDED NEWLINE
+len = . - hellowrld # COUNTS CHARS INCLUDING \n
+
+# NEW TEXT DATA
+hello2: .ascii "YOU TOO"
+new2: .byte 0x0A
+l2 = . - hello2
 
 .text
 _start:
@@ -15,6 +20,13 @@ mov $1, %rdi # SPECIFY STD OUT (TERMINAL)
 mov $hellowrld, %rsi # SEND DATA ADRESS
 mov $len, %rdx # SPECIFY STRINGS LENGTH
 syscall # CALLING THE WRITE FUNCTION
+
+# ANOTHER WRITE CALL
+mov $1, %rax
+mov $1, %rdi
+mov $hello2, %rsi
+mov $l2, %rdx
+syscall
 
 # SAFE EXITING
 mov $60, %rax
