ab4e91a67be34b962996c8cf06bbc6bdd0b0af26
AdamPodymniak1 <kamilgrzecznarowski@gmail.com>
2026-03-05 23:29:08 +0100

    hello world program!

--- main.s
+++ main.s
@@ -1,9 +1,22 @@
 .global _start
 
-.text
+.data
 
+hellowrld: .ascii "HELLO, WORLD!" # CREATE DATA POINT
+newline: .byte 0x0A
+len = . - hellowrld
+
+.text
 _start:
 
+# HELLO WORLD CODE:
+mov $1, %rax # SPECIFY WRITING MODE
+mov $1, %rdi # SPECIFY STD OUT (TERMINAL)
+mov $hellowrld, %rsi # SEND DATA ADRESS
+mov $len, %rdx # SPECIFY STRINGS LENGTH
+syscall # CALLING THE WRITE FUNCTION
+
+# SAFE EXITING
 mov $60, %rax
 xor %rdi, %rdi
 syscall
