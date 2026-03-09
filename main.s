b8ee944baafe953a73c19853a1d69b7528ba2bc0
AdamPodymniak1 <kamilgrzecznarowski@gmail.com>
2026-03-09 19:03:11 +0100

    added comparison methods

--- main.s
+++ main.s
@@ -1,32 +1,45 @@
 .global _start
 
 .data
-
-str1: .ascii "Printing"
-new_lime: .byte 0x0A
+str1: .ascii "Printing\n"
 s1_len = . - str1
 
 .text
 _start:
-
-mov $0, %rbx
+    mov $0, %rbx
 
 loop:
-cmp $3, %rbx # COMPARE TWO VALUES
-jge exit # JUMP if GREATER OR EQUAL (je - JUMP if EQUAL, jne = JUMP if NOT EQUAL)
-call print
-inc %rbx # INCREMENT RAX. Alternative: add $1, %rax
-jmp loop # CREATED LOOP
+    # WAYS TO COMPARE
+    # 1. Register vs Constant: cmp $3, %rbx
+    # 2. Register vs Register: cmp %rcx, %rbx
+    # 3. Register vs Memory:   cmp count_limit(%rip), %rbx
+    
+    cmp $3, %rbx
+    
+    # CONDITIONAL JUMPS (Check EFLAGS register)
+    # jge = Greater or Equal (Signed)
+    # jae = Above or Equal   (Unsigned - use for addresses)
+    # jle = Less or Equal
+    # je  = Equal / jne = Not Equal
+    # jb  = Below / jbe = Below or Equal
+    
+    jge exit
+
+    call print
+
+    inc %rbx
+    jmp loop
 
 print:
-mov $1, %rax
-mov $1, %rdi
-mov $str1, %rsi
-mov $s1_len, %rdx
-syscall
-ret
+    mov $1, %rax
+    mov $1, %rdi
+    mov $str1, %rsi
+    mov $s1_len, %rdx
+    syscall
+    
+    ret
 
 exit:
-mov $60, %rax
-xor %rdi, %rdi
-syscall
+    mov $60, %rax
+    xor %rdi, %rdi
+    syscall
